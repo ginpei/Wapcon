cb8b11c978f4f01d0e6db186c5054e0d1699c61c
ginpei <ginpei@gmail.com>
2017-09-14 17:42:53 -0700

    remove debug stuff

--- front/screens/Home/index.vue
+++ front/screens/Home/index.vue
@@ -2,13 +2,6 @@
 	BaseLayout.row
 		ThemeColumn.item.main
 		MachineColumn.item.aside(:executingMachine="executingMachine" :machineOn="machineOn" :onToggleMachine="onToggleMachine")
-		div
-			div
-				button(@click="sleep0" style="height:50px") Error
-			div
-				button(@click="sleep1" style="height:50px") Sleep 1 sec
-			div
-				button(@click="sleep3" style="height:50px") Sleep 3 sec
 </template>
 
 <style lang="sass" scoped>
@@ -28,9 +21,6 @@
 	const BaseLayout = require('../../components/BaseLayout/index.vue')
 	const ThemeColumn = require('./ThemeColumn.vue')
 	const MachineColumn = require('./MachineColumn.vue')
-
-	const bridge = require('../../lib/bridge.js')
-	const { ipcRenderer } = window.electron
 
 	module.exports = {
 		components: {
@@ -120,20 +110,6 @@
 
 				// TODO handle error
 			},
-
-			sleep0(event) {
-				bridge('sleep', { delay: 0 })
-					.catch(error => console.error('error', error))
-			},
-			sleep1(event) {
-				bridge('sleep', { delay: 1000 })
-					.then(result => console.log('done', result))
-			},
-			sleep3(event) {
-				bridge('sleep', { delay: 3000 })
-					.then(result => console.log('done', result))
-			},
-
 		},
 	}
 </script>
