f1c0671abcdc6e2c8a17b117f243d0f6098143ec
ginpei <ginpei@gmail.com>
2017-09-16 18:48:44 -0700

    button UI

--- front/components/form/GButton.vue
+++ front/components/form/GButton.vue
@@ -6,10 +6,20 @@
 <style lang="sass" scoped>
 	button
 		border: 1px solid #999
-		box-shadow: 1px 1px 1px rgba(255, 255, 255, 0.99) inset
+		box-shadow: 1px 1px 1px rgba(255, 255, 255, 0.9) inset
 		min-height: 50px
-		text-shadow: 1px 1px rgba(255, 255, 255, 0.5)
 		min-width: 50px
+		padding: 1px
+		text-shadow: 1px 1px rgba(255, 255, 255, 0.5)
+		vertical-align: middle
+
+		&:active
+			box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.2) inset
+			padding: 2px 0 0 2px
+
+		&:disabled
+			box-shadow: none
+			color: #999
 </style>
 
 <script>
