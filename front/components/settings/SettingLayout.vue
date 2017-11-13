ff579f1fb18e013c8f44e29b2402b5b7648b1415
ginpei <ginpei@gmail.com>
2017-11-12 23:49:28 -0800

    extract layout

---
+++ front/components/settings/SettingLayout.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+	div.layout
+		div.header
+			div.header-inner
+				router-link.back(to="/")
+					i.fa.fa-arrow-left
+					| Back
+				h1.title {{title}}
+
+		main.main
+			slot
+</template>
+
+<style lang="sass" scoped>
+	.layout
+		background-color: #fff
+
+	.header
+		border-bottom: 1px solid #999
+
+	.header-inner
+		padding: 1em
+		max-width: 600px
+		margin: 0 auto
+
+	.back
+		background-color: #efe
+		color: #060
+		display: inline-block
+		padding: .5em 1em
+
+	.title
+		margin: 0
+		padding: 0 1rem
+
+	.main
+		padding: 1em
+		max-width: 600px
+		margin: 0 auto
+</style>
+
+<script>
+	module.exports = {
+		props: [
+			'title',
+		],
+	}
+</script>
--- front/screens/Preferences/index.vue
+++ front/screens/Preferences/index.vue
@@ -1,74 +1,41 @@
 <template lang="pug">
-	BaseLayout.baseLayout
-		div.header
-			div.header-inner
-				router-link.back(to="/")
-					i.fa.fa-arrow-left
-					| Back
-				h1.title Preferences
+	SettingLayout(title="Preferences")
+		section
+			SettingHeading Database
+			SettingTable
+				tr
+					th Path
+					td
+						SettingInput(:value="$store.state.preferences.databasePath" readonly)
+				tr
+					th Reset
+					td
+						GIconButton(icon="exclamation-triangle") Delete all
 
-		main.main
-			section
-				SettingHeading Database
-				SettingTable
-					tr
-						th Path
-						td
-							SettingInput(:value="$store.state.preferences.databasePath" readonly)
-					tr
-						th Reset
-						td
-							GIconButton(icon="exclamation-triangle") Delete all
+		section
+			SettingHeading WordPress
+			SettingTable
+				tr
+					th Path
+					td
+						input.preference-input(:value="$store.state.preferences.wordpressPath" readonly)
+				tr
+					th Download
+					td
+						a(href="https://wordpress.org/download/")
+							i.fa.fa-external-link
+							|  WordPress.org
 
-			section
-				SettingHeading WordPress
-				SettingTable
-					tr
-						th Path
-						td
-							input.preference-input(:value="$store.state.preferences.wordpressPath" readonly)
-					tr
-						th Download
-						td
-							a(href="https://wordpress.org/download/")
-								i.fa.fa-external-link
-								|  WordPress.org
-
-			section
-				SettingHeading Data
-				SettingTable
-					tr
-						th Reset
-						td
-							GIconButton(:onPress="revert_onClick" icon="undo") Revert
+		section
+			SettingHeading Data
+			SettingTable
+				tr
+					th Reset
+					td
+						GIconButton(:onPress="revert_onClick" icon="undo") Revert
 </template>
 
 <style lang="sass" scoped>
-	.baseLayout
-		background-color: #fff
-
-	.header
-		border-bottom: 1px solid #999
-
-	.header-inner
-		padding: 1em
-		max-width: 600px
-		margin: 0 auto
-
-	.back
-		background-color: #efe
-		color: #060
-		display: inline-block
-		padding: .5em 1em
-
-	.title
-		margin: 0
-		padding: 0 1rem
-
-	.main
-		padding: 1em
-		max-width: 600px
-		margin: 0 auto
 </style>
 
 <script>
@@ -76,6 +43,7 @@
 	const GIconButton = require('../../components/form/GIconButton.vue')
 	const SettingHeading = require('../../components/settings/SettingHeading.vue')
 	const SettingInput = require('../../components/settings/SettingInput.vue')
+	const SettingLayout = require('../../components/settings/SettingLayout.vue')
 	const SettingTable = require('../../components/settings/SettingTable.vue')
 
 	module.exports = {
@@ -84,6 +52,7 @@
 			GIconButton,
 			SettingHeading,
 			SettingInput,
+			SettingLayout,
 			SettingTable,
 		},
 
